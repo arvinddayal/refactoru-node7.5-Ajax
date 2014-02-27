f9d8a17d26e3e74d9e10de1ac873598662fd415b
arvindayal <arvinddayal@gmail.com>
2014-02-27 11:23:45 -0700

    part I really done

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,18 @@
 $(function(){
-	
+
+
 
 	$("#my-button").click(function(){
 		$.ajax('/countries',{
 			success:function(data){
 				for (var i = 0; i < data.length; i++) {
-					$('body').append(data[i].name);
+					var newList = $('<ul>');
+					var liName = $('<li>Name: {0}</li>'.supplant([data[i].name]));
+					var liFrench = $('<li>French Name: {0}</li>'.supplant([data[i].frenchName]));
+					var liLocal = $('<li>Local Name: {0}</li>'.supplant([data[i].localName]));
+					var region = $('<li>Region: {0}</li>'.supplant([data[i].region]));
+					var newCountry = newList.append(liName, liFrench, liLocal, region);
+					$('body').append(newCountry);
 				}
 			}
 		});
