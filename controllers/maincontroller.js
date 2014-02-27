9d7de574d8af94c8700eae3884cfcf33eff90fc9
arvindayal <arvinddayal@gmail.com>
2014-02-27 16:51:04 -0700

    real time fuzzy search added

--- controllers/maincontroller.js
+++ controllers/maincontroller.js
@@ -4,11 +4,12 @@ var UserController = module.exports = {
 
 	filter: function(req, res){
 		var x = req.query;
+		var y = [];
 		for (var i = 0; i < UserModel.length; i++) {
-			if(UserModel[i].name === x.search) {
-				res.send(UserModel[i]);
+			if (((UserModel[i].name).toUpperCase().indexOf((x.search).toUpperCase())) != (-1)) {
+				y.push(UserModel[i]);
 			}
 		}
+		res.send(y);
 	}
 };
-
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -20,20 +20,20 @@ $(function(){
 	});
 
 //Search Function
-	$('#search').click(function(e){
-		e.preventDefault();
+	$('#searchbox').keydown(function(){
 		$.ajax('/search', {
-			data: $(this).prev().serialize(),
+			data: $(this).serialize(),
 			success:function(data){
-				console.log(data);
 				$('.country-display').empty();
+				for (var i = 0; i < data.length; i++) {
 				var newList = $('<ul>');
-				var liName = $('<li>Name: {0}</li>'.supplant([data.name]));
-				var liFrench = $('<li>French Name: {0}</li>'.supplant([data.frenchName]));
-				var liLocal = $('<li>Local Name: {0}</li>'.supplant([data.localName]));
-				var region = $('<li>Region: {0}</li>'.supplant([data.region]));
+				var liName = $('<li>Name: {0}</li>'.supplant([data[i].name]));
+				var liFrench = $('<li>French Name: {0}</li>'.supplant([data[i].frenchName]));
+				var liLocal = $('<li>Local Name: {0}</li>'.supplant([data[i].localName]));
+				var region = $('<li>Region: {0}</li>'.supplant([data[i].region]));
 				var newCountry = newList.append(liName, liFrench, liLocal, region);
 				$('.country-display').append(newCountry);
+				}
 			}
 		});
 
